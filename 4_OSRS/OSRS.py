c9e1c9b28e172f76050d0f825857e57b9a3c8ee3
Connor9994 <ConnorKaiser@proton.me>
2024-12-04 01:05:56 -0800

    modified:   4_OSRS/OSRS.py

--- 4_OSRS/OSRS.py
+++ 4_OSRS/OSRS.py
@@ -3,6 +3,9 @@ import autoit
 import os
 import math
 import statistics
+import cv2
+import numpy as np
+from PIL import Image 
 
 # Bring WoW Client to the Foreground
 # autoit.win_activate("RuneLite")
@@ -22,35 +25,51 @@ def calculate_angle(center, point):
     # Calculate angle in radians
     angle_radians = math.atan2(delta_y, delta_x)
 
-    # Convert to degrees if needed
+    # Convert to degrees and adjust rotation
     angle_degrees = math.degrees(angle_radians)
     angle_degrees = (angle_degrees + 90) % 360
 
-    return angle_radians, angle_degrees
+    return angle_degrees
 
 while True:
     try:
-        CompassX, CompassY, CompassWidth, CompassHeight = pyautogui.locateOnScreen("Compass.png", confidence=0.75)
+        CompassX, CompassY, CompassWidth, CompassHeight = pyautogui.locateOnScreen("Compass.png", confidence=0.7)
         if CompassX:
-            CompassX, CompassY, CompassWidth, CompassHeight = int(CompassX), int(CompassY), int(CompassWidth), int(CompassHeight)
-            #print(str(CompassX) + ", " + str(CompassY) + ", " + str(CompassWidth) + ", " + str(CompassHeight))
-
-            image = pyautogui.screenshot(region=(CompassX, CompassY, CompassWidth, CompassHeight))
-            image.save("CompassCurrent.png")
+            CompassX, CompassY, = int(CompassX), int(CompassY)
+            CompassWidth, CompassHeight = int(CompassWidth), int(CompassHeight)
+            Compass = pyautogui.screenshot(region=(CompassX, CompassY, CompassWidth, CompassHeight))
+            Compass.save("CompassCurrent.png")
             XArray= []
             YArray=[]
             for x in range(CompassWidth):
                 for y in range(CompassHeight):
-                    current_color = image.getpixel((x, y))
+                    current_color = Compass.getpixel((x, y))
                     if current_color == (49, 41, 29):
                         XArray.append(x)
                         YArray.append(y)
-                        break
 
             x = statistics.mean(XArray)
             y = statistics.mean(YArray)
-            angle_radians, angle_degrees = calculate_angle(((CompassWidth/2), (CompassHeight/2)), (x, y))
-            #print("Found Color at: " + str(x - CenterX) + ", " + str(CompassY - y))
+            angle_degrees = calculate_angle(((CompassWidth/2), (CompassHeight/2)), (x, y))
             print("Angle of Map: " + str(round(angle_degrees,1)))
+
+            Minimap = pyautogui.screenshot(region=(int(CompassX + (CompassWidth/2) + 25), CompassY + 29, 110, 110))
+            Minimap.save("Minimap.png")
+
+            Minimap = Image.open("Minimap.png").rotate(angle_degrees,3)
+            Minimap.save("Minimap_Rotated.png")
+            
+            WorldMap = cv2.imread("WorldMap.jpg")
+            Minimap = cv2.imread("Minimap_Rotated.png")
+            res = cv2.matchTemplate(WorldMap, Minimap, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(res >= .3)
+
+            Length = 0
+            for pt in zip(*loc[::-1]):
+                if (Length == 0):
+
+                    print(pt[0] + ", " + pt[1])
+                    Length = 1
+                
     except Exception as e:
-        print("No Compass")
+        print(e)
